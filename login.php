afdd893bbea0a4a276275154251aa020680932d6
Qichenye <59810094+Qichenye@users.noreply.github.com>
2020-06-26 16:43:07 -0500

    login and signup

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+<body>
+    <form action="login.php" method="POST" >
+        <span class="big">Username:</span>
+        <input type="text" name="username" id="username"/>
+        <span class="big">Password:</span>
+        <input type="password" name="password" id="password" />
+        <input type="submit" value="Login" name="submit" class="login_page_buttons"/>
+    </form>
+
+    <?php
+        if(isset($_POST['submit'])&&$_POST['username']!=null){
+            $username=(String)$_POST['username'];
+            $input_password=(String)$_POST['password'];
+            require 'database.php';
+
+            $stmt = $mysqli->prepare("select * from users where username='$username'");
+            if(!$stmt){
+                printf("Query Prep Failed: %s\n", $mysqli->error);
+                exit;
+            }
+    
+            $stmt->execute();
+            $result= $stmt->get_result();
+            $row = $result->fetch_assoc();
+            $hashed_password = $row['password'];
+    
+            if(password_verify($input_password, $hashed_password)){
+                session_start();
+                $_SESSION["username"]=$username;
+                header("Location: main.php");
+                exit;
+            }
+    
+            $stmt->close();
+        }
+    ?>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up</title>
+</head>
+<body>
+    <form action="login.php" method="POST" >
+        <span class="big">Username:</span><br>
+        <input type="text" name="username" id="username"/> <br><br>
+        <span class="big">Password:</span><br>
+        <input type="password" name="password" id="password" /> <br><br>
+        <span class="big">Confirm Password:</span><br>
+        <input type="password" name="confirm_password" id="password" />
+        <input type="submit" value="Signup" name="submit" class="login_page_buttons"/>
+    </form>
+
+
+    <?php
+        require 'database.php';
+        if(isset($_POST['submit'])&&$_POST['username']!=null&&$_POST['password']!=null){
+            if($_POST['password']!=$_POST['confirm_password']){
+                echo "Password Does Not Match";
+            }
+            else{
+                $username = (String)$_POST['username'];
+                $hashed_password = password_hash((String)$_POST['password'], PASSWORD_DEFAULT);
+
+                $stmt = $mysqli->prepare("insert into users (username, password) values (?, ?)");
+                if(!$stmt){
+                    printf("Query Prep Failed: %s\n", $mysqli->error);
+                    exit;
+                }
+
+                $stmt->bind_param('ss', $username, $hashed_password);
+                
+                $stmt->execute();
+
+                $stmt->close();
+            }
+        }
+    ?>
+
+
+
+</body>
+</html>
